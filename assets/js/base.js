e4e6ed792fad2810b2f077cacf0133a53edc86aa
Corina <corinap@hotmail.com>
2024-06-14 14:17:40 -0300

    function carregaPrevisao, criação base.js

---
+++ assets/js/base.js
@@ -0,0 +1,118 @@
+
+
+
+const loadHeader = () =>{
+    let el = document.getElementById('header')
+
+    el.innerHTML ='<div class="container d-flex align-items-center justify-content-between">'+
+
+      '<div class="logo">'+
+        '<h1 class="text-light"><a href="index.html"><img src="assets/img/logo/daee_up.png"></a></h1>'+
+      '</div>'+
+
+      '<nav id="navbar" class="navbar">'+
+        '<ul>'+
+          '<li><a class="active" href="index.html">Home</a></li>'+
+          '<li><a href="noticias.html">Notícias</a></li>'+
+          '<li class="dropdown"><a href="#"><span>Sala de Situação</span> <i class="bi bi-chevron-down"></i></a>'+
+
+            '<ul>'+
+              '<li><a href="sobre.html">Sobre</a></li>'+
+              '<li><a href="salas-de-situacao.html">Salas de Situação</a></li>'+
+              '<li><a href="monitoramento.html">Monitoramento</a></li>'+
+              '<li><a href="equipe.html">Equipe</a></li>'+
+              '<li><a href="galeria.html"> Galeria</a></li>'+
+            '</ul>'+
+          '</li>'+
+
+          '<li class="dropdown"><a href="#"><span>Boletins</span> <i class="bi bi-chevron-down"></i></a>'+
+            '<ul>'+
+              '<li><a href="https://drive.google.com/drive/folders/0B4yicqLa_Dj8YTE5ZDUyNTItMjkzYS00ZGJlLTg2M2ItZTI0ZjRjODQ3ZDNk?resourcekey=0-n6Kjkz-jNDVJdgI1dJ1A1A" target="_black">Boletim Diário</a></li>'+
+              '<li><a href="https://drive.google.com/drive/folders/0B4yicqLa_Dj8VFFVb3B4bW5xZkk?resourcekey=0-9rWDTUK16xaKOQnDIPFlnw" target="_black">Boletim Mensal</a></li>'+
+              '<li><a href="https://docs.google.com/document/d/11qxQPqNc1RW7mMYw_uXdw-Gmh7lr8UUbaH2AnG6k6g0/edit#heading=h.7vzq6ltz512f" target="_black">Último Boletim</a></li>'+
+            '</ul>'+
+          '</li>'+
+          '<li><a href="estacoes.html">Estações</a></li>'+
+          '<li><a href="hidrologia.html">Hidrologia</a></li>'+
+        
+
+        '</ul>'+
+        '<i class="bi bi-list mobile-nav-toggle"></i>'+
+      '</nav>'+
+
+    '</div>';
+
+}
+
+const loadFooter = () =>{
+    let rodape = document.getElementById('footer')
+
+    rodape.innerHTML = 
+
+    '<div class="footer-top">'+
+    '<div class="container">'+
+      '<div class="row">'+
+
+        '<div class="col-lg-3 col-md-6 footer-info">'+
+          '<h3>SSSP-DAEE</h3>'+
+          '<p>Rede Hidrológica básica do Estado de São Paulo é operada pelo DAEE desde 1951.</p>'+
+          '<p>Sala de Situação de São Paulo, apresenta produtos e relatos por redes sociais e e-mails. </p>'+
+        '</div>'+
+
+        '<div class="col-lg-3 col-md-6 footer-links">'+
+          '<h4>Boletins</h4>'+
+          '<ul>'+
+            '<li><a href="#">Boletins Diários</a></li>'+
+            '<li><a href="#">Boletins Mensais</a></li>'+
+            '<li><a href="#">Ultimos Boletim</a></li>'+
+          '</ul>'+
+           '<h4>Sobre</h4>'+
+           '<ul>'+
+            '<li><a href="sobre.php">Sobre Nós</a></li>'+
+            '<li><a href="equipe.php">Equipes</a></li>'+
+          '</ul>'+
+        '</div>'+
+        '<div class="col-lg-3 col-md-6 footer-links">'+
+          '<h4>Situação</h4>'+
+          '<ul>'+
+            '<li><a href="#">Chuvas Diárias Alto Tietê/Cubatão</a></li>'+
+            '<li><a href="#">Últimos Alertas</a></li>'+
+            '<li><a href="#">Ultimas Publicações da SSSP / DAEE</a></li>'+
+          '</ul>'+
+        '</div>'+
+
+        '<div class="col-lg-3 col-md-6 footer-contact">'+
+          '<h4>Contato</h4>'+
+          '<p>'+
+            'Rua Boa Vista, 170 <br>'+
+            '11º andar, Bloco 5<br>'+
+           'São Paulo / SP <br>'+
+            '<strong>Telefone:</strong> (11) 3293 - 8461<br>'+
+            '<strong>Email:</strong> salasituacao@daee.sp.gov.br<br>'+
+          '</p>'+
+
+          '<div class="social-links"> <p>Redes Sociais DAEE</p>'+
+            '<a href="https://www.facebook.com/DAEESP/" target="_black" class="facebook"><i class="bi bi-facebook"></i></a>'+
+            '<a href="https://www.instagram.com/daeesp/" target="_black" class="instagram"><i class="bi bi-instagram"></i></a>'+
+            
+
+            '<a href="https://www.youtube.com/channel/UCOEBfJpTq7LE25rdjX9tdUQ" target="_black" class=""><i class="bi bi-youtube"></i></a>'+
+          '</div>'+
+
+        '</div>'+
+
+      '</div>'+
+    '</div>'+
+  '</div>'+
+
+  '<div class="container">'+
+    '<div class="copyright">'+
+      '&copy;  <strong><span>SSSP-DAEE</span></strong>. '+
+    '</div>'+
+    '<div class="credits">'+
+    '</div>'+
+  '</div>';
+}
+
+loadHeader()
+loadFooter()
--- assets/js/index.js
+++ assets/js/index.js
@@ -2,61 +2,69 @@
 
 window.onload = function(){
   
-    //carregarTabelaDado()
-    carregarRelatos()
-
+    // carregarTabelaDado()
+    // carregarRelatos()
+    //  carregarRelatosModal()
+    // CarregaMap()
+    // categorizarValores()
+    carregaPrevisao()
+     
 }
 
-
-
 function carregarTabelaDado(){
-    fetch("https://cors-anywhere.herokuapp.com/https://cth.daee.sp.gov.br/sibh/api/v1/measurements/last_hours_events?hours=24&show_all=true").then(function(response){
-        response.json().then(function(dados){
-            console.log(dados);
-            let medicoes = dados.json
+   fetch("https://cors-anywhere.herokuapp.com/https://cth.daee.sp.gov.br/sibh/api/v1/measurements/last_hours_events?hours=24&show_all=true").then(function(response){
+         response.json().then(function(dados){
+             console.log(dados);
+             let medicoes = dados.json
 
             //ordenar e cortar as  maiores
-            let maiores = medicoes.sort((a,b)=>b.value-a.value).slice(0,5)
-
+             let maiores = medicoes.sort((a,b)=>b.value-a.value).slice(0,8)
 
             //intancia tabela
             let tabela = document.getElementById("tabela_eventos")
             
             //instancia corpo tabela
-            let tbody = tabela.getElementsByTagName("tbody")[0]
+           let tbody = tabela.getElementsByTagName("tbody")[0]
 
-            //apaga os dados da tabela
+             //apaga os dados da tabela
             tbody.innerHTML = null
             
             //para cada medicao..
-            maiores.forEach(maior => {
+             maiores.forEach(maior => {
 
-                //criar linha tabela
+                 //criar linha tabela
                 let tr = document.createElement("tr")
 
                 //preencher linha tabela
-                tr.innerHTML = "<td>"+maior.city+"</td><td>"+maior.value.toFixed(1)+"</td><td>-</td><td>-</td>"
+                tr.innerHTML = "<td>"+maior.city+
+                "</td><td>"+maior.value.toFixed(1)+
+                "</td><td>"+maior.station_prefix_id+
+                "</td><td>"+maior.value.toFixed(1)
+                +"</td>"
 
                 //append = anexar linha no corpo da tabela
-                tbody.appendChild(tr)
+                 tbody.appendChild(tr)
             });
 
-            if(maiores.length == 0){
+             if(maiores.length == 0){
                 let tr = document.createElement("tr")
 
                 tr.innerHTML = "<td colspan='4'>Sem Dados</td>"
 
-                tbody.appendChild(tr)
-            }
+                 tbody.appendChild(tr)
+             }
 
+      
         })
-    })
-}
+      })
+   }
 
-function carregarRelatos(){
-    fetch("https://cors-anywhere.herokuapp.com/https://cth.daee.sp.gov.br/sibh/api/v1/control_rooms/1/get_reports?limit=20&offset=0").then(function(respostaRelatos){
-         respostaRelatos.json().then(function(relatos){
-            //let relatos = []
+
+ function carregarRelatos(){
+    
+    fetch("https://cors-anywhere.herokuapp.com/https://cth.daee.sp.gov.br/sibh/api/v1/control_rooms/1/get_reports?limit=20&offset=0").then(function(response){
+         response.json().then(function(relatos){
+            // let relatos = []
             console.log(relatos);
 
             let relato = relatos[0]
@@ -66,7 +74,7 @@ function carregarRelatos(){
             let card= document.getElementById("card_relatos")
             card.innerHTML = null
 
-            textoRelato.innerHTML = "<div>"+relato.start_date+"</div><div>"+relato.desc+"</div>"
+            textoRelato.innerHTML = "<div><b>" +relato.start_date+"</b></div><div>"+relato.desc+"</div>"
 
             card.appendChild(textoRelato)
 
@@ -75,7 +83,89 @@ function carregarRelatos(){
 
        
      })
+
+}
+function carregaPrevisao(){
+
+    let latLngs = [
+        {text:'-23.624199, -46.676390', name:'Centro'},
+        {text:'-23.556239, -46.424378', name:'Zona Leste'}
+    ]
+
+    latLngs.forEach(latLng=>{
+        fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+latLng.text+"?unitGroup=metric&key=F25Q6KUDD73CMRSWWS8US97X6&contentType=json").then(function(response){
+            response.json().then(function(previsao){
+                console.log(previsao);
+
+                let climaAgora = previsao.currentConditions            
+
+                let previsaoTempo = document.createElement("div") 
+                previsaoTempo.innerHTML = "<div>"+latLng.name+"</div><div>"+climaAgora.datetime+"</div><div>"+climaAgora.temp +"</div>"
+
+                let prev = document.getElementById("previsao")
+
+                prev.appendChild(previsaoTempo)
+
+            })
+        })
+    })
+
+    
 }
 
 
+
+
+
+
+
+// function CarregaMap(){
+//     // Inicializar o mapa
+//     var map = L.map('map').setView([-23.5505, -46.6333], 10); // São Paulo
+  
+//     // Adicionar a camada de mapa
+//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//     }).addTo(map);
+
+//     // Adicionar marcador
+//     // var marker = L.marker([-23.5505, -46.6333]).addTo(map);
+//     // marker.bindPopup("<b>São Paulo</b><br>Capital do estado de São Paulo.").openPopup();
+// }
+
+
+
+// Array de valores para categorizar e colorir
+// const valores = [0, 2, 6, 11, 3, 8, 15];
+
+// // Função para categorizar e colorir os valores
+// function categorizarValores(valores) {
+//     const tabela = document.getElementById('valores');
+    
+//     valores.forEach(valor => {
+//         const tr = document.createElement('tr');                                                 
+        
+//         const tdValor = document.createElement('td');
+//         tdValor.textContent = valor;
+
+//         const tdCategoria = document.createElement('td');
+
+//         if (valor > 1 && valor <= 5) {
+//             tdCategoria.textContent = 'Fraca';
+//             tdCategoria.className = 'fraca';
+//         } else if (valor > 5 && valor <= 10) {
+//             tdCategoria.textContent = 'Moderada';
+//             tdCategoria.className = 'moderada';
+//         } else if (valor > 10) {
+//             tdCategoria.textContent = 'Forte';
+//             tdCategoria.className = 'forte';
+//         } else {
+//             tdCategoria.textContent = 'N/A';
+//         }
+
+//         tr.appendChild(tdValor);
+//         tr.appendChild(tdCategoria);
+//         tabela.appendChild(tr);
+//     });
+// }
 
